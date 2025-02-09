de217cff57a5ba93f5e2517eea9347df624c485d
Rafee <rafeehasan88@gmail.com>
2025-02-10 02:35:25 +0600

    basic route and firebase setup

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpskQNqNk4C3gfmOyCtfihjd7IXlgQ3cA",
+  authDomain: "react-auth-and-private-r-73dee.firebaseapp.com",
+  projectId: "react-auth-and-private-r-73dee",
+  storageBucket: "react-auth-and-private-r-73dee.firebasestorage.app",
+  messagingSenderId: "479494253505",
+  appId: "1:479494253505:web:a53ebd2188053f9f535d40"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
